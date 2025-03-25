d4e0797a2f1890c4dba348b6bf1ba2c976d0dde8
245122749053 <245122749053@mvsrec.edu.in>
2025-03-25 15:59:51 +0530

    add1.java

--- add1.java
+++ add1.java
@@ -2,11 +2,10 @@ import java.util.*;
 class add1{
 	public static void main(String args[]){
 		int a,b,c;
-		Scanner myobj=new Scanner(System.in);
+		int a=Integer.parse.Int(args[0]);
+		int b=Integer.parse.Int(args[1]);
 		System.out.println("enter a number");
-		a=myobj.nextInt();
 		System.out.println("enter a number");
-		b=myobj.nextInt();
 		int add=a+b;
 		int sub=a-b;
 		int mul=a*b;
